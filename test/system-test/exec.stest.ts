0f5489d397d422d57dda3664e3f06122eab4bc56
Dennis Meister <dennis.meister@bosch.com>
2023-02-23 11:27:02 +0100

    Test system test
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- test/system-test/exec.stest.ts
+++ test/system-test/exec.stest.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { spawnSync } from 'child_process';
 import { join } from 'path';
+import YAML from 'yaml';
 
 const VELOCITAS_PROCESS = join('..', '..', process.env['VELOCITAS_PROCESS'] ? process.env['VELOCITAS_PROCESS'] : 'velocitas');
 
@@ -11,6 +12,14 @@ describe('CLI command', () => {
             spawnSync(VELOCITAS_PROCESS, ['init']);
         });
         it('should be able to install dapr', () => {
+            const packageOutput = spawnSync(VELOCITAS_PROCESS, ['package', 'runtime-local'], {
+                stdio: 'inherit',
+            });
+            console.log('Test packageOutput');
+            console.log(packageOutput.stdout);
+            console.log(packageOutput.stdout.toString());
+            YAML.parse(packageOutput.stdout.toString());
+
             const output = spawnSync(VELOCITAS_PROCESS, ['exec', 'runtime-local', 'ensure-dapr'], {
                 stdio: 'inherit',
             });
