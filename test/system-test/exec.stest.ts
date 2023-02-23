0ca1e42b49b94c4735a776dd7cd658946b97dc23
Dennis Meister <dennis.meister@bosch.com>
2023-02-23 13:53:39 +0100

    Test on branch
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- test/system-test/exec.stest.ts
+++ test/system-test/exec.stest.ts
@@ -17,7 +17,7 @@ describe('CLI command', () => {
             const runtimeLocalComponent = parsedPackageOutput['devenv-runtime-local'].components.find(
                 (component: any) => component.id === 'runtime-local'
             );
-            let spawnSuccesful: any;
+            let spawnSuccesful = false;
             for await (const exposedProgramSpec of runtimeLocalComponent.programs) {
                 console.log(`Try to spawn exposed program of 'runtime-local': ${exposedProgramSpec.id}`);
                 const processSpawn = spawn(VELOCITAS_PROCESS, ['exec', 'runtime-local', exposedProgramSpec.id], {
@@ -33,24 +33,16 @@ describe('CLI command', () => {
     });
 });
 
-const checkSpawn = async (exposedProgramSpecId: string, processSpawn: ChildProcess): Promise<boolean | undefined> => {
-    const spawnSucces = await new Promise((resolve, reject) => {
+const checkSpawn = async (exposedProgramSpecId: string, processSpawn: ChildProcess): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
         processSpawn.on('spawn', () => {
             console.log(`Spawned ${exposedProgramSpecId} succesfully - killing process`);
             processSpawn.kill();
             resolve;
         });
-    });
-    const spawnError = await new Promise((resolve, reject) => {
         processSpawn.on('error', () => {
             console.log(`Spawning ${exposedProgramSpecId} resulted in an error`);
-            resolve;
+            reject;
         });
     });
-    if (spawnError) {
-        return false;
-    }
-    if (spawnSucces) {
-        return true;
-    }
 };
