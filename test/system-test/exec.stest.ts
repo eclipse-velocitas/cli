084797ae0313cc5ec26b9826bec21459694f8617
Dominic Sudy <dominic.sudy@bosch.com>
2023-02-24 14:21:05 +0100

    Add system test for env variables

--- test/system-test/exec.stest.ts
+++ test/system-test/exec.stest.ts
@@ -14,17 +14,24 @@
 
 import { expect } from 'chai';
 import { spawn, spawnSync } from 'child_process';
+import { copySync } from 'fs-extra';
+import { homedir } from 'node:os';
 import { join } from 'path';
+import { cwd } from 'process';
 import YAML from 'yaml';
 
 const VELOCITAS_PROCESS = join('..', '..', process.env['VELOCITAS_PROCESS'] ? process.env['VELOCITAS_PROCESS'] : 'velocitas');
+const TEST_ROOT = cwd();
+const VELOCITAS_HOME = `${homedir()}/.velocitas`;
 
 describe('CLI command', () => {
     describe('exec', () => {
-        before(() => {
-            process.chdir('./testbench/test-exec');
+        beforeEach(() => {
+            process.chdir(`${TEST_ROOT}/testbench/test-exec`);
+            copySync('./packages', `${VELOCITAS_HOME}/packages`);
             spawnSync(VELOCITAS_PROCESS, ['init']);
         });
+
         it('should be able to exec all exposed program specs of runtime-local', async () => {
             const packageOutput = spawnSync(VELOCITAS_PROCESS, ['package', 'devenv-runtime-local'], { encoding: 'utf-8' });
             const parsedPackageOutput = YAML.parse(packageOutput.stdout.toString());
@@ -38,6 +45,14 @@ describe('CLI command', () => {
                 continue;
             }
             expect(spawnSuccesful).to.be.true;
+        });
+
+        it('should pass environment variables to the spawned process', async () => {
+            const result = spawnSync(VELOCITAS_PROCESS, ['exec', 'test-component', 'echo-env'], { encoding: 'utf-8' });
+
+            expect(result.stdout).to.contain('VELOCITAS_WORKSPACE_DIR=');
+            expect(result.stdout).to.contain('VELOCITAS_CACHE_DATA=');
+            expect(result.stdout).to.contain('VELOCITAS_CACHE_DIR=');
         });
     });
 });
