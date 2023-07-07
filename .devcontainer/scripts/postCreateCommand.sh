3e0e0a1db396bb13e36221cf847a81ebe3f368d8
Dennis Meister <dennis.meister@bosch.com>
2023-07-07 10:32:27 +0200

    Revert merge
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Copyright (c) 2023 Robert Bosch GmbH
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Clone python template into devContainer to act as a testing playground for CLI development
+echo "#######################################################"
+echo "###        Clone vehicle-app-python-template        ###"
+echo "###         to act as a testing playground          ###"
+echo "###               for CLI execution                 ###"
+echo "#######################################################"
+
+rm -rf vehicle-app-repo/
+git clone --depth=1 https://github.com/eclipse-velocitas/vehicle-app-python-template.git vehicle-app-repo
+
+# add repo to git safe.directory
+REPO=$(pwd)
+git config --global --add safe.directory $REPO
+
+echo "#######################################################"
+echo "###  Install prerequisites for development of CLI   ###"
+echo "###    Node, build-essentials, npm dependencies     ###"
+echo "#######################################################"
+
+./install_node.sh
+rm -rf node_modules/
+sudo apt-get -y install build-essential
+npm ci
+
+# Create alias to development CLI binary for testing inside devContainer
+echo "alias velocitas=$REPO/bin/dev" >> ~/.zshrc
+echo "alias velocitas=$REPO/bin/dev" >> ~/.bashrc
+
+# Don't let container creation fail if lifecycle management fails
+echo "Done!"
--- NOTICE-3RD-PARTY-CONTENT.md
+++ NOTICE-3RD-PARTY-CONTENT.md
@@ -4,42 +4,42 @@
 | Dependency | Version | License |
 |:-----------|:-------:|--------:|
 |@oclif/core|1.26.2|MIT|
-|@oclif/plugin-help|5.2.9|MIT|
-|@oclif/plugin-not-found|2.3.23|MIT|
+|@oclif/plugin-help|5.2.11|MIT|
+|@oclif/plugin-not-found|2.3.26|MIT|
 |@oclif/plugin-plugins|2.4.7|MIT|
-|@oclif/test|2.3.19|MIT|
+|@oclif/test|2.3.26|MIT|
 |@types/archiver|5.3.2|MIT|
 |@types/chai|4.3.5|MIT|
 |@types/decompress|4.2.4|MIT|
 |@types/fs-extra|11.0.1|MIT|
 |@types/mocha|10.0.1|MIT|
 |@types/mock-fs|4.13.1|MIT|
-|@types/node|18.16.7|MIT|
-|@typescript-eslint/eslint-plugin|5.59.5|MIT|
-|@typescript-eslint/parser|5.59.5|unknown|
+|@types/node|18.16.19|MIT|
+|@typescript-eslint/eslint-plugin|5.61.0|MIT|
+|@typescript-eslint/parser|5.61.0|unknown|
 |archiver|5.3.1|MIT|
 |axios|0.27.2|MIT|
-|axios-retry|3.4.0|unknown|
+|axios-retry|3.5.1|unknown|
 |chai|4.3.7|MIT|
 |decompress|4.2.1|MIT|
-|eslint|8.40.0|MIT|
+|eslint|8.44.0|MIT|
 |mocha|10.2.0|ISC<br/>MIT|
 |mock-fs|5.2.0|MIT|
 |node-pty|0.10.1|MIT|
 |nyc|15.1.0|ISC<br/>MIT|
-|oclif|3.9.0|MIT|
+|oclif|3.9.1|MIT|
 |pkg|5.8.1|MIT|
 |prettier|2.8.8|Apache 2.0|
 |prettier-plugin-organize-imports|3.2.2|MIT|
 |recursive-copy|2.0.14|ISC|
-|semver|7.5.0|ISC|
+|semver|7.5.3|ISC|
 |shx|0.3.4|MIT|
-|sinon|15.0.4|MIT|
+|sinon|15.2.0|MIT|
 |ts-node|10.9.1|MIT|
-|tslib|2.5.0|BSD Zero Clause License|
+|tslib|2.6.0|BSD Zero Clause License|
 |typescript|4.9.5|Apache 2.0|
 |velocitas-cli|0.5.0|Apache 2.0|
-|yaml|2.2.2|ISC|
+|yaml|2.3.1|ISC|
 ## Workflows
 | Dependency | Version | License |
 |:-----------|:-------:|--------:|
